f2ce4cb8a771a2b8667d2ad926361554a8994a2b
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-05-23 17:46:04 -0300

    Create models en home: Post y Autor

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    título = models.CharField(max_length=100)
+    contenido = models.TextField()
+
+    def __str__(self):
+        return self.título
+
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
