0c3709ed49fa5c066ecd504562e9ea5ac3ea2287
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2023-05-23 18:18:03 -0300

    Create forms y templates en home: Autor y Post

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from . import forms
 
 
 def index(request):
     return render(request, "home/index.html")
+
+
+def crear_autor(request):
+    if request.method == "POST":
+        form = forms.AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form = forms.AutorForm()
+    return render(request, "home/crear_autor.html", {"form": form})
+
+
+def crear_post(request):
+    if request.method == "POST":
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+    else:
+        form = forms.PostForm()
+    return render(request, "home/crear_post.html", {"form": form})
